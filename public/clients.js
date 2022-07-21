c5d18433ffdca06d9c0950c3954c1ce57b20655d
m7mdmakkeh <m.makkeh@cognativex.com>
2022-07-21 18:06:26 +0300

    asd

--- public/clients.js
+++ public/clients.js
@@ -1,34 +1,63 @@
 const clients = [
     {
-        htmlpagename: "striveme.html",
         name: "StriveMe",
-        logo: "https://striveme.com/img/front/logo.png"
+        logo: "https://striveme.com/img/front/logo.png",
+        widgets: [
+            {
+                id: 5756975958720512,
+                htmlpagename: "striveme(اخترنا لك).html",
+            },
+            {
+                id: 5642933638266880,
+                htmlpagename: "striveme(الاكثر قراءة).html",
+            },
+
+        ]
     },
     {
-        htmlpagename: "yawmiyati-subpage.html",
         name: "يومياتي",
         logo: "https://yawmiyati.com/assets/site/images/logo.svg",
         widgets: [
             {
-                id: 0,
+                id: 5683908364468224,
                 htmlpagename: "yawmiyati-home.html",
             },
-            {
-                id: 0,
+            { 
+                id: 5769997225820160,
                 htmlpagename: "yawmiyati-subpage.html",
             },
         ]
     },
-    {
-        htmlpagename: "alittihad.html",
-        name: "الأتحاد",
-        logo: "https://static.alittihad.ae/front/Content/Alittihad/img/logo-desktop.svg"
-    },
-    {
-        htmlpagename: "aljarida.html",
-        name: "الجريدة",
-        logo: "https://www.aljarida.com/i/app_icon_ar.png"
-    },
+    // {
+    //     htmlpagename: "alittihad.html",
+    //     name: "الأتحاد",
+    //     logo: "https://static.alittihad.ae/front/Content/Alittihad/img/logo-desktop.svg",
+    //     widgets: [
+    //         {
+    //             id: 0,
+    //             htmlpagename: "yawmiyati-home.html",
+    //         },
+    //         {
+    //             id: 1,
+    //             htmlpagename: "yawmiyati-subpage.html",
+    //         },
+    //     ]
+    // },
+    // {
+    //     htmlpagename: "aljarida.html",
+    //     name: "الجريدة",
+    //     logo: "https://www.aljarida.com/i/app_icon_ar.png",
+    //     widgets: [
+    //         {
+    //             id: 0,
+    //             htmlpagename: "yawmiyati-home.html",
+    //         },
+    //         {
+    //             id: 1,
+    //             htmlpagename: "yawmiyati-subpage.html",
+    //         },
+    //     ]
+    // },
 
 
 ]
